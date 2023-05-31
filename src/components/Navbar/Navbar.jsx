e5dff3d1cf880fbd0e3066ee43f9d920da6af2a2
Andrew Bonnell <abonnell1005@gmail.com>
2023-05-30 18:25:47 -0700

    navbar comp for layout

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,37 @@
+"use client";
 import Link from "next/link";
+import { AppBar, Container, Toolbar, Box, Button } from "@mui/material";
+import { usePathname } from "next/navigation";
+
+import styles from "./navbar.module.css";
 
 export default function Navbar({ navLinks }) {
 	const navArr = Object.values(navLinks);
+	const path = usePathname();
+
 	return (
-		<div>
-			{navArr.map((item, index) => {
-				console.log(item);
-				return (
-					<Link key={`${item.key}-${index}`} href={item.path}>
-						{item.key}
-					</Link>
-				);
-			})}
-		</div>
+		<AppBar>
+			<Container>
+				<Toolbar>
+					{/* Icon */}
+					<Box>
+						{navArr.map((item, index) => {
+							const classes = `${styles.navItem} ${
+								path === item.path ? styles.currentPage : ""
+							}`;
+							return (
+								<Link
+									key={`${item.key}-${index}`}
+									href={item.path}
+									className={classes}
+								>
+									{item.key.toUpperCase()}
+								</Link>
+							);
+						})}
+					</Box>
+				</Toolbar>
+			</Container>
+		</AppBar>
 	);
 }
