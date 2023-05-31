a531c441f520ea27c35668a2328b62aa95e53f22
Andrew Bonnell <abonnell1005@gmail.com>
2023-05-30 21:19:46 -0700

    update to exploding dx

--- src/utils/dice.js
+++ src/utils/dice.js
@@ -2,13 +2,13 @@ export function rolldX(die) {
   return Math.floor(Math.random() * die) + 1;
 }
 
-export function explodingSixes(input = 0, history = []) {
-  let result = input + rolldX(6);
+export function explodingdXs(dieSize, input = 0, history = []) {
+  let result = input + rolldX(dieSize);
   history = [...history, result - input];
 
   // Number is multiple of 6
   if (result % 6 === 0) {
-    return explodingSixes(result, history);
+    return explodingdXs(dieSize, result, history);
   } else {
     return { result, history };
   }
