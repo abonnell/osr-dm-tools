35f4bd4b541a0e89e2bc4da2c4fbe7add21f3604
Andrew Bonnell <abonnell1005@gmail.com>
2023-05-30 19:59:09 -0700

    bugfix +=; history

--- src/utils/dice.js
+++ src/utils/dice.js
@@ -1,14 +1,15 @@
 export function rolldX(die) {
-	return Math.floor(Math.random() * die) + 1;
+  return Math.floor(Math.random() * die) + 1;
 }
 
-export function explodingSixes(input = 0) {
-	let result = (input += rolldX(6));
+export function explodingSixes(input = 0, history = []) {
+  let result = input + rolldX(6);
+  history = [...history, result - input];
 
-	// Number is multiple of 6
-	if (result % 6 === 0) {
-		return explodingSixes(result);
-	} else {
-		return result;
-	}
+  // Number is multiple of 6
+  if (result % 6 === 0) {
+    return explodingSixes(result, history);
+  } else {
+    return { result, history };
+  }
 }
